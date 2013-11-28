5f67b36099bd327b2c1bd7e136e0080c4e5fc8f3
Chen Xu <xuchen81@chens-mbp.wireless.yale.internal>
2013-11-27 22:42:04 -0500

    Added util class

---
+++ util.cpp
@@ -0,0 +1,10 @@
+#include "util.h"
+
+Util::Util()
+{
+}
+
+QString Util::construct32bitsHashId(QString md5HashResult) {
+    QString kk = "sdf";
+    return kk;
+}
---
+++ util.h
@@ -0,0 +1,29 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <QVBoxLayout>
+#include <QApplication>
+#include <QDebug>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <QDialog>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QUdpSocket>
+#include <QPushButton>
+#include <QVariant>
+#include <QVariantMap>
+#include <QLocalServer>
+#include <QFileDialog>
+#include <QtCrypto>
+
+
+class Util
+{
+public:
+    Util();
+    static QString construct32bitsHashId(QString md5HashResult);
+};
+
+#endif // UTIL_H
