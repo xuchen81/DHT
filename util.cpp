b5db06c8542e4c0b8b4b49a00a9a1bf0a25acb0b
Chen Xu <xuchen81@chens-mbp.wireless.yale.internal>
2013-12-03 06:00:46 -0500

    Stooped randomizing serverId every time

--- util.cpp
+++ util.cpp
@@ -10,12 +10,23 @@ QString Util::getHashId(QString k) {
     QByteArray h = QCA::Hash("md5").hash(byteArray).toByteArray();
     QString md5re = h.toHex();
 
-    srand(time(NULL));
     QString bit32Hash = "";
+    bit32Hash.append(md5re.at(29));
+    bit32Hash.append(md5re.at(23));
+    bit32Hash.append(md5re.at(8));
+    bit32Hash.append(md5re.at(25));
+    bit32Hash.append(md5re.at(27));
+    bit32Hash.append(md5re.at(11));
+    bit32Hash.append(md5re.at(0));
+    bit32Hash.append(md5re.at(5));
+
+    /* Stopped randomizing everytime, because searching a key will give key different ID
+    srand(time(NULL));
     for (int i = 0; i < 8; i ++) {
         int r = qrand() % 32;
+        qDebug() << "random: " << r;
         bit32Hash.append(md5re.at(r));
     }
-
+    */
     return bit32Hash;
 }
